df42e08205514003113ce633009824f98100d7b9
Nikolay Belokonenko <belokonenkons@gmail.com>
2023-09-09 23:55:56 +0300

    Monitor manager refactoring remove 4th cenral screen add objects

--- MonitorsManager.cpp
+++ MonitorsManager.cpp
@@ -1,1 +1,26 @@
 #include "MonitorsManager.h"
+
+MonitorsManager::MonitorsManager()
+{
+    // ----------------- Monitors state ----------------------
+    GLFWmonitor** monitorsPtr = glfwGetMonitors(&monitorsCount);
+    std::cout << "Monitors curently connected: " << monitorsCount << std::endl;
+    if (!monitorsPtr) {
+        std::cout << "No monitors were found!" << std::endl;
+        glfwTerminate();
+        throw std::runtime_error("No monitors were found!");   //Refactoring !!! Add tray/catch
+    }
+
+    for (int monitorIndex{ 0 }; monitorIndex < monitorsCount; monitorIndex++)
+    {
+        MonitorData data;
+        data.monitor = *(monitorsPtr + monitorIndex);
+        glfwGetMonitorWorkarea(data.monitor, &data.monitor_X, &data.monitor_Y, &data.monitor_Width, &data.monitor_Height);
+        monitors.push_back(data);
+    }
+}
+
+const MonitorData MonitorsManager::getMonitor(int windowIndex) const
+{
+    return monitors.at(windowIndex);
+}
